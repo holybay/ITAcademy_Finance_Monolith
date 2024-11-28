0b6fd7ed495782e685bdafe826118588e3fead31
Vlad Golubev <holybay1886@gmail.com>
2024-11-28 15:17:28 +0200

    Updated OperationRepository interface.

--- finance-app/src/main/java/by/it_academy/jd2/finance/repository/IAccountOperationRepository.java
+++ finance-app/src/main/java/by/it_academy/jd2/finance/repository/IAccountOperationRepository.java
@@ -1,16 +1,15 @@
 package by.it_academy.jd2.finance.repository;
 
 import by.it_academy.jd2.finance.repository.entity.AccountOperation;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-import java.math.BigDecimal;
-import java.util.Optional;
 import java.util.UUID;
 
 public interface IAccountOperationRepository extends JpaRepository<AccountOperation, UUID> {
 
-    @Query("SELECT SUM(o.value) from AccountOperation o WHERE o.account.id = :id")
-    Optional<BigDecimal> sumByAccountId(@Param("id") UUID id);
+    Page<AccountOperation> findAllByAccountId(Pageable pageable, UUID accountId);
+
+    long countByAccountId(UUID accountId);
 }
