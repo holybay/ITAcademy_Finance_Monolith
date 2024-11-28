1f9f6f01516b7cee494d606085161332d7c3126b
Vlad Golubev <holybay1886@gmail.com>
2024-11-28 20:31:36 +0200

    Added auditing user login event.

--- finance-app/src/main/java/by/it_academy/jd2/finance/service/impl/AuthService.java
+++ finance-app/src/main/java/by/it_academy/jd2/finance/service/impl/AuthService.java
@@ -1,10 +1,13 @@
 package by.it_academy.jd2.finance.service.impl;
 
 import by.it_academy.jd2.finance.platform.exception.AppAuthException;
+import by.it_academy.jd2.finance.repository.entity.auditUnit.EEssenceType;
 import by.it_academy.jd2.finance.repository.entity.user.EUserStatus;
 import by.it_academy.jd2.finance.repository.entity.user.User;
+import by.it_academy.jd2.finance.service.IAuditService;
 import by.it_academy.jd2.finance.service.IAuthService;
 import by.it_academy.jd2.finance.service.IUserService;
+import by.it_academy.jd2.finance.service.dto.auditUnit.AuditUnitCreateDto;
 import by.it_academy.jd2.finance.service.dto.user.UserLoginDto;
 import by.it_academy.jd2.finance.service.util.JwtTokenHandler;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -16,11 +19,13 @@ public class AuthService implements IAuthService {
     private final IUserService userService;
     private final PasswordEncoder encoder;
     private final JwtTokenHandler tokenHandler;
+    private final IAuditService auditService;
 
-    public AuthService(IUserService userService, PasswordEncoder encoder, JwtTokenHandler tokenHandler) {
+    public AuthService(IUserService userService, PasswordEncoder encoder, JwtTokenHandler tokenHandler, IAuditService auditService) {
         this.userService = userService;
         this.encoder = encoder;
         this.tokenHandler = tokenHandler;
+        this.auditService = auditService;
     }
 
     @Override
@@ -30,6 +35,12 @@ public class AuthService implements IAuthService {
         if (user.getStatus() == EUserStatus.DEACTIVATED || !encoder.matches(loginDto.getPassword(), user.getPassword())) {
             throw new AppAuthException("Incorrect mail or password!");
         }
+        auditService.create(AuditUnitCreateDto.builder()
+                                              .setUserId(user.getId())
+                                              .setText("User logged in!")
+                                              .setType(EEssenceType.USER)
+                                              .setEssenceTypeId(user.getId())
+                                              .build());
         return tokenHandler.generateToken(user);
     }
 }
