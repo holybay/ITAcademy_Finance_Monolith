a9e474d4acfec2f75631715e5908e07c3c95d3a7
Vlad Golubev <holybay1886@gmail.com>
2024-11-29 16:43:36 +0200

    Adjusted .sql scripts names

--- postgresql/sql/003.sql
+++ postgresql/sql/003.sql
@@ -1,3 +1,5 @@
+\c finance_monolith
+
 INSERT INTO app.currencies (id, title, description, created_at, updated_at)
 VALUES (gen_random_uuid(), 'USD', 'United Stated Dollar', now(), now()),
 (gen_random_uuid(), 'EUR', 'European Union Mark', now(), now()),
@@ -6,3 +8,10 @@ VALUES (gen_random_uuid(), 'USD', 'United Stated Dollar', now(), now()),
 
 INSERT INTO app.operation_categories (id, title, created_at, updated_at)
 VALUES (gen_random_uuid(), 'Default', now(), now());
+
+INSERT INTO app.users (id, mail, full_name, role, status, password, created_at, updated_at)
+VALUES (gen_random_uuid(), 'admin@finance.com', 'adminus admin', 'ADMIN', 'ACTIVATED',
+'$2a$12$TOmvpka3W8a2lpHundeHge9u8m2Tf1RtuKz4lyoULbPRyqrfUN9SK', now(), now()),
+
+(gen_random_uuid(), 'user@finance.com', 'ordinary user', 'USER', 'ACTIVATED',
+'$2a$10$Vulfz0xB4gGbRGNoZSjKXOwV265cebZQfn.2VtU9x9lfXZgb36GY.', now(), now());
